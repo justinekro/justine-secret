e2f20ec156fe4e5d0b08b53ffebc79760797f497
Justine <>
2018-02-05 11:55:08 +0100

    debut crud et css

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,13 +3,16 @@ class UsersController < ApplicationController
   	@user = User.new
   end
 
+  def index
+  end
+
   def edit
   	@user = User.find(params[:id])
   end
 
   def create
   	@user = User.new(user_params)
-  	if user.save
+  	if @user.save
   		redirect_to @user
   	else render 'new'
   	end
@@ -18,7 +21,7 @@ class UsersController < ApplicationController
   def update
   	@user = User.find(params[:id])
   	if @user.update(user_params)
-  		redirect_to @article
+  		redirect_to @user
   	else render 'edit'
   	end
   end
@@ -35,7 +38,7 @@ class UsersController < ApplicationController
 
 	private
   	def user_params
-    	params.permit(:name, :email)
+    	params.permit(:name, :email, :password)
   	end
 
 end
