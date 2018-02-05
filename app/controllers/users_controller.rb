a60e609c6b0b516e6c3a50c05ef275f9b0158679
Justine <>
2018-02-05 10:20:13 +0100

    Make a basic User model (including secure passwords)

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,41 @@
+class UsersController < ApplicationController
+  def new
+  	@user = User.new
+  end
+
+  def edit
+  	@user = User.find(params[:id])
+  end
+
+  def create
+  	@user = User.new(user_params)
+  	if user.save
+  		redirect_to @user
+  	else render 'new'
+  	end
+  end
+
+  def update
+  	@user = User.find(params[:id])
+  	if @user.update(user_params)
+  		redirect_to @article
+  	else render 'edit'
+  	end
+  end
+
+	def destroy
+		@user = User.find(params[:id])
+		@user.destroy
+		redirect_to users_path
+	end
+
+	def show
+		@user = User.find(params[:id])
+	end
+
+	private
+  	def user_params
+    	params.permit(:name, :email)
+  	end
+
+end
