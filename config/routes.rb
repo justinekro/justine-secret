3f330727857665d0a7f29546ddbded756f8dd816
Justine <>
2018-02-05 13:21:28 +0100

    added static pages

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
-  root 'users#index'
+  root 'static_pages#index'
+  get 'answer', to: 'static_pages#show'
   resources :users
+
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
