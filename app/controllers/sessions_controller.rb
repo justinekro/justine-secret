ff32095491230ad6bd9d7630c010649dfcd2f95f
Justine <>
2018-02-06 11:42:53 +0100

    added cookies and remember me checkbox

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -7,6 +7,7 @@ class SessionsController < ApplicationController
     if user && user.authenticate(params[:session][:password])
       flash[:success] = "Vous êtes connecté ! Bienvenue sur le site qui vous donnera la réponse à la question de l'univers..."     
       log_in user
+      params[:session][:remember_me] == '1' ? remember(user) : forget(user)
       redirect_to user
     else 
 
@@ -17,9 +18,10 @@ class SessionsController < ApplicationController
 
 
   def destroy
-    log_out
+    log_out if logged_in?
     flash[:danger] = 'Vous êtes bien déconnecté'
     redirect_to root_url
   end
+
 
 end
